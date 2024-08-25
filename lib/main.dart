f368ae775f1bdec8fcb24f54eca8c677b36ea1a6
votiendung1 <dungvt.21it@vku.udn.vn>
2024-08-25 22:27:20 +0700

    I am Rich

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IamRich());
+}
+
+class IamRich extends StatelessWidget {
+  const IamRich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.amber.shade700,
+          appBar: AppBar(
+            backgroundColor: Colors.amber.shade900,
+          title: const Text(
+            "I AM RICH", 
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Image(
+              image: AssetImage("images/1297123.png"),
+              width: 350,
+            ),
+          ],
+          ),
+        ),
+        )
+      ),
+    );
+
+  }
+}
